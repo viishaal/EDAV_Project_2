7cf8d254f2275da50150600cc26f4d04d863f8b6
vishal <vj2208@columbia.edu>
2016-02-23 22:03:49 -0500

    attempt at ggplot and larger world map

--- code.R
+++ code.R
@@ -4,8 +4,9 @@
 library("ggmap")
 library("maptools")
 library("maps")
-
 library(RNetCDF)
+require(reshape2)
+
 fname = "data/NOAA_Daily_phi_500mb.nc"
 fid = open.nc(fname)
 print.nc(fid)
@@ -15,19 +16,44 @@ dat = read.nc(fid)
 #map("world", fill=TRUE, col="white", ylim=c(-60, 90), mar=c(0,0,0,0))
 data("wrld_simpl")
 lon = dat$X - 180
-lat = at$Y
+lat = dat$Y
 phi = dat$phi[,,1]
-quartz(width=11, height=4)
-image(lon, rev(lat), phi, xlab="Longitude", ylab="Latitude")
+plot.new()
+quartz(width=9, height=6)
+#bwr = colorRampPalette(c("blue", "white", "red"))
+image(lon, rev(lat), phi, xlab="Longitude", ylab="Latitude", xlim=c(-180,180), ylim=c(-90,90))
+#map("world", add = TRUE)
 #box()
 #contour(x, y, phi, add=TRUE, levels = seq(90, 200, by = 5))
 title(main="Phi")
 plot(wrld_simpl, add=TRUE)
+#map("world", fill=TRUE, col="white", bg="lightblue", ylim=c(-60, 90), mar=c(0,0,0,0), add=T)
+#mapWorld <- borders("world", colour="gray50", fill="gray50")
+#mapWorld
 
 
 ################## try ggplot
+df = data.frame(phi)
+names(df) = lat
+df$lon= lon
+mdata = melt(df, id=c("lon"))
+names(mdata) = c("lon", "lat", "x")
+mdata$x = as.numeric(mdata$x)
+mdata$lon = as.numeric(mdata$lon)
+mdata$lat = as.numeric(as.character(mdata$lat))
 
+wr <- map_data("world")
+# Prepare a map of World
+wrmap <- ggplot(wr, aes(x = long, y = lat, group = group)) +
+  geom_polygon(fill = "white", colour = "black") +
+  geom_point(data=mdata, inherit.aes=FALSE, aes(x=lon, y=lat, fill=x), size=3, shape=4) +
+  #ggplot(mdata, inherit.aes=F, aes(lon, lat))
+  scale_fill_gradient("Phi", limits=c(4500,6000)) +
+  #scale_fill_brewer()
+  theme_bw() +
+  coord_equal()
 
+wrmap
 
 ################# make an animation
 # get an years data
